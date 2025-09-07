64c3bcda64ea68f290fdce2441a60d0bdf0463c3
Dario Zubaray <dzubaray@gmail.com>
2025-09-07 12:10:21 -0300

    agregando actividad integradora nro 1 y solucion 'ProgramacionOrientadaObjetos'

---
+++ src/ProgramacionOrientaObjetos/TPI1-Itinerario4/Alumno.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TPI1_Itinerario4
+{
+    class Alumno : ICloneable
+    {
+        public int Legajo { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        private DateTime _fechaNacimiento { get; set; }
+        public DateTime FechaNacimiento { set { _fechaNacimiento = value; } }
+        private DateTime _fechaIngreso { get; set; }
+        public DateTime FechaIngreso { set { _fechaIngreso = value; } }
+        public int Edad { get; }
+        public bool Activo { get; set; }
+        private int _cantMateriasAprobadas { get; set; }
+        public int CantMateriaAprobadas { set { _cantMateriasAprobadas = value; } }
+
+        public Alumno() { }
+        public Alumno(int legajo, string nombre, string apellido, DateTime fechaNacimiento)
+        {
+            Legajo = legajo;
+            Nombre = nombre;
+            Apellido = apellido;
+            _fechaNacimiento = fechaNacimiento;
+            _fechaIngreso = DateTime.Now;
+            Edad = (int)((DateTime.Now - fechaNacimiento).Days / 365.25);
+            Activo = true;
+            _cantMateriasAprobadas = 0;
+        }
+
+        ~Alumno()
+        {
+            Console.WriteLine("Liberando legajo: " + this.Legajo);
+        }
+
+        public string NombreCompleto()
+        {
+            return $"{Nombre} {Apellido}";
+        }
+
+        public int Antiguedad(string unidad)
+        {
+            DateTime hoy = DateTime.Today;
+            TimeSpan diferencia = hoy - _fechaIngreso;
+
+            switch (unidad.ToLower())
+            {
+                case "d":
+                case "dias":
+                    return diferencia.Days;
+                case "m":
+                case "meses":
+                    return (int)(diferencia.TotalDays / 30);
+                case "y":
+                case "años":
+                    return (int)(diferencia.TotalDays / 365.25);
+
+                default:
+                    return 0;
+            }
+        }
+
+        public int MateriasNoAprobadas()
+        {
+            int TotalMaterias = 36; // Este dato depende de la carrera. Por ahora aqui esta bien
+            return TotalMaterias - _cantMateriasAprobadas;
+        }
+
+        public int EdadDeIngreso()
+        {
+            return (int)Math.Round((_fechaIngreso - _fechaNacimiento).TotalDays / 365.25);
+        }
+
+        public object Clone() => this.MemberwiseClone();
+        public Alumno CloneTipado() => Clone() as Alumno;
+
+        public static List<Alumno> CopiaListaAlumnos(List<Alumno> alumnos)
+        {
+            List<Alumno> aux = new List<Alumno>();
+            foreach (var alumno in alumnos)
+            {
+                aux.Add(alumno.CloneTipado());
+            }
+            return aux;
+        }
+    }
+}
---
+++ src/ProgramacionOrientaObjetos/TPI1-Itinerario4/Form1.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace TPI1_Itinerario4
+{
+    public partial class Form1 : Form
+    {
+        List<Alumno> ListaAlumnos;
+        public Form1()
+        {
+            InitializeComponent();
+            ListaAlumnos = new List<Alumno>();
+            dataGridView1.DataSource = ListaAlumnos;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.MultiSelect = false;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+        }
+
+        private void Mostrar(DataGridView pDGV, object pO)
+        {
+            pDGV.DataSource = null; pDGV.DataSource = pO;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string title = "Registro de alumno";
+            string nombre = Interaction.InputBox("Ingrese nombre:", title, "");
+            string apellido = Interaction.InputBox("Ingrese apellido:", title, "");
+            DateTime fechaNacimiento;
+            try
+            {
+                string fechaNacimientoIngresada = Interaction.InputBox("Ingrese fecha de nacimiento:", title, "dd/mm/yyyy");
+                fechaNacimiento = DateTime.ParseExact(fechaNacimientoIngresada, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+            } catch
+            {
+                fechaNacimiento = DateTime.Now;
+            }
+
+            int nuevoLegajo = ListaAlumnos.Any() ? ListaAlumnos.Max(a => a.Legajo) + 1 : 1;
+            Alumno nuevoAlumno = new Alumno(nuevoLegajo, nombre, apellido, fechaNacimiento);
+            ListaAlumnos.Add(nuevoAlumno);
+
+            Mostrar(dataGridView1, Alumno.CopiaListaAlumnos(ListaAlumnos));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.Rows.Count == 0) return;
+
+            int legajoABorrar = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells[0].Value);
+            Alumno alumnoABorrar = ListaAlumnos.Find(alumno => alumno.Legajo.Equals(legajoABorrar));
+
+            var result = MessageBox.Show($"Seguro que quiere borrar a {alumnoABorrar.NombreCompleto()}", "Confirmar", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (result.Equals(DialogResult.Yes))
+            {
+                ListaAlumnos.Remove(alumnoABorrar);
+                Mostrar(dataGridView1, Alumno.CopiaListaAlumnos(ListaAlumnos));
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.Rows.Count == 0) return;
+
+            int legajoAModificar = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells[0].Value);
+            Alumno alumnoAModificar = ListaAlumnos.Find(alumno => alumno.Legajo.Equals(legajoAModificar));
+            if(alumnoAModificar == null)
+            {
+                MessageBox.Show("Ocurrio un error al modificar el alumno.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            string title = "Modificación de alumno";
+            string nuevoNombre = Interaction.InputBox("Ingrese nombre:", title, alumnoAModificar.Nombre);
+            string nuevoApellido = Interaction.InputBox("Ingrese apellido:", title, alumnoAModificar.Apellido);
+            string nuevaFechaNacimiento = Interaction.InputBox("Ingrese fecha de nacimiento:", title, "");
+            string nuevaCantMateriasAprobadas = Interaction.InputBox("Ingrese cantidad de materias aprobadas:", title, "0");
+            alumnoAModificar.Nombre = nuevoNombre;
+            alumnoAModificar.Apellido = nuevoApellido;
+            alumnoAModificar.FechaNacimiento = DateTime.ParseExact(nuevaFechaNacimiento, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+            alumnoAModificar.CantMateriaAprobadas = Convert.ToInt32(nuevaCantMateriasAprobadas);
+        }
+
+        private void dataGridView1_CellMouseEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                var fila = dataGridView1.Rows[e.RowIndex];
+                var alumnoMouseEnter = fila.DataBoundItem as Alumno;
+
+                if (alumnoMouseEnter != null)
+                {
+                    textBox1.Text = alumnoMouseEnter.Antiguedad("años").ToString();
+                    textBox2.Text = alumnoMouseEnter.MateriasNoAprobadas().ToString();
+                    textBox3.Text = alumnoMouseEnter.EdadDeIngreso().ToString();
+                }
+            }
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow != null)
+            {
+                Alumno seleccionado = dataGridView1.CurrentRow.DataBoundItem as Alumno;
+                if (seleccionado != null)
+                {
+                    textBox1.Text = seleccionado.Antiguedad("años").ToString();
+                    textBox2.Text = seleccionado.MateriasNoAprobadas().ToString();
+                    textBox3.Text = seleccionado.EdadDeIngreso().ToString();
+                }
+            }
+        }
+    }
+}
